1f58a591df0f025217c6714103d9a441d30d2880
siti fadillah <2511530012@mahasiswa.atmaluhur.ac.id>
2026-01-07 15:46:32 -0800

    Ucapan Otomatis Saat Halaman Dimuat (jS)

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -8,6 +8,13 @@ document.getElementById("menuToggle").addEventListener("click", function () {
     } 
 });
 
+document.addEventListener("DOMContentLoaded", function () { 
+const homeSection = document.getElementById("home"); 
+const ucapan = document.createElement("p"); 
+ucapan.textContent = "Halo! Selamat datang di halaman saya!"; 
+homeSection.appendChild(ucapan); 
+}); 
+
 document.querySelector("form").addEventListener("submit", function (e) { 
   const nama = document.getElementById("txtNama"); 
   const email = document.getElementById("txtEmail"); 
