7927fb939cbe88e16e7006a8b48f489f093dde1b
siti fadillah <2511530012@mahasiswa.atmaluhur.ac.id>
2026-01-13 16:16:54 -0800

    update index.php pertemuan-16

--- pertemuan-16/bahan uas pwd bly/index.php
+++ pertemuan-16/bahan uas pwd bly/index.php
@@ -33,7 +33,7 @@ require_once __DIR__ . '/fungsi.php';
       <h2>Selamat Datang</h2>
       <?php
       echo "halo dunia!<br>";
-      echo "nama saya siti ";
+      echo "nama saya siti fadillah ";
       ?>
       <p>Ini contoh paragraf HTML.</p>
     </section>
